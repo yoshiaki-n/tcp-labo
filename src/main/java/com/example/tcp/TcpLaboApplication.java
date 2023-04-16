1bbe7437e007df8a1c76daaa23661eaa114e4615
yoshiaki-n <n52844@gmail.com>
2023-04-16 10:24:52 +0900

    最初のコミット

---
+++ src/main/java/com/example/tcp/TcpLaboApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpLaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpLaboApplication.class, args);
+	}
+
+}
